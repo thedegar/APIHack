a9793ffb9025ec3ef02d5ffd56276816c7e9a972
thedegar <tchedegard@gmail.com>
2014-06-26 20:00:13 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/*-----------------------------------
+//	Tyler Hedegard
+//	06-26-14
+//	Thinful.com API Hack Project
+//	The DOW NOW
+//---------------------------------*/
+
+$(document).ready( function() {
+	
+});
+
+$(document).ready(function() {
+	
+});

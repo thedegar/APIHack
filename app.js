435b90c7d92e73946a92d039f53e2ea3e4afc9c4
thedegar <tchedegard@gmail.com>
2014-07-15 22:15:10 -0500

    JS additions
    Added click on header to sort and reorder the stocks into a new
    variable finalStocks.

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ $(document).ready(function() {
 	}
 });
 
+$(document).on("click","#header",function() {
+	if (stockData.length < 30) {
+		alert("Please wait.  The qoutes are not complete yet.");
+	}
+	findGains();
+	reorderStocks();
+});
+
 //Ticker symbols for eadh Dow company
 var dow = [
 	"MMM","AXP","T","BA","CAT","CVX","CSCO","DD","XOM",
@@ -21,6 +29,11 @@ var dow = [
 	"MSFT","NKE","PFE","PG","KO","TRV","UTX","UNH","VZ",
 	"V","WMT","DIS"
 ];
+
+//global variables
+var stockData = [];
+var gains = [];
+var finalStocks = [];
 
 //AJAX call to get real time information on a stock ticker
 var quotes = function(ticker) {
@@ -33,16 +46,11 @@ var quotes = function(ticker) {
 		})
 	.done(function(quote){
 		storeData(quote);
-		//need code to order by % gain
-
 	})
 	.fail(function(jqXHR, error, errorThrown){
 		console.log(error);
 	});
 };
-
-//Initiate main variable where the data will be stored
-var stockData = [];
 
 function Stock(ticker, exchange, price, change, changePercent) {
 	this.ticker = ticker;
@@ -54,5 +62,58 @@ function Stock(ticker, exchange, price, change, changePercent) {
 
 var storeData = function(quote) {
 	stockData.push(new Stock(quote[0].t, quote[0].e, quote[0].l, quote[0].c, quote[0].cp));
-	console.log(stockData);
 };
+
+//function finds the gains for each stock quote and orders them max to min
+var findGains = function() {
+	gains = [];
+	for (var i=0;i<stockData.length;i++) {
+		gains.push(eval(stockData[i].changePercent));
+	}
+	gains.sort().reverse();
+	//Sort doesn't order negative numbers properly
+	//This code finds the negative values and reorders them
+	var negGains = [];
+	for (var i=29;i>=0;i--) {
+		if (gains[i]<0) {
+			negGains.push(gains[i]);
+			gains.pop();
+		}
+	}
+	gains = gains.concat(negGains);
+};
+
+//Function reorders the stock array in order of descending change percent
+var reorderStocks = function() {
+	for (var i=0;i<gains.length;i++) {
+		//Variable to handle duplicate gains
+		var duplicateCount = 0;
+		//Count how many matching gains
+		for (var z=0;z<gains.length;z++) {
+			if (gains[i]==gains[z]) {
+				duplicateCount++;
+			}
+		}
+		//Find stocks that match the gains amount
+		for (var j=0;j<stockData.length;j++) {
+			if (gains[i] == eval(stockData[j].changePercent)) {
+				finalStocks.push(stockData[j]);
+				//handle duplicate gains
+				while (duplicateCount > 1) {
+					for (var l=j+1;l<stockData.length;l++) {
+						if (gains[i] == eval(stockData[l].changePercent)) {
+							finalStocks.push(stockData[l]);
+							duplicateCount--; //decrement count 
+							j++; //increment j to finish finding duplicates
+							i++; //increment i by 1 since you just pushed another value
+							break; //break out of loop
+						}
+					}
+				}
+				break;  //break out of loop
+			}
+		}
+	}
+};
+
+
